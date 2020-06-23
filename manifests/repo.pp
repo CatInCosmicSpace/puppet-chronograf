757e1687504204d4bbce08ef21987be43126ed13
Woth-Ursula <uwoth@kogito.network>
2020-06-23 09:23:32 +0200

    repo with apt-get update 2
    Signed-off-by: Woth-Ursula <uwoth@kogito.network>

--- manifests/repo.pp
+++ manifests/repo.pp
@@ -8,58 +8,24 @@ class chronograf::repo (
   String $package_name = $chronograf::package_name,
 ){
 
-  if $manage_repo {
-
-    case $facts['os']['distro']['id'] {
-      'Ubuntu': {
-        apt::source { 'influxdata':
-          comment  => 'InfluxDB repository',
-          location => 'https://repos.influxdata.com/ubuntu',
-          release  => $facts[os][distro][codename],
-          repos    => 'stable',
-          key      => {
-            'id'     => '05CE15085FC09D18E99EFB22684A14CF2582E0C5',
-            'source' => 'https://repos.influxdata.com/influxdb.key',
-          },
-        }
-        exec { 'chronograf_apt_get_update':
-          command     => 'apt-get update',
-          cwd         => '/tmp',
-          path        => ['/usr/bin'],
-          require     => Apt::Source['influxdata'],
-          subscribe   => Apt::Source['influxdata'],
-          refreshonly => true,
-        }
-
-        package { $package_name:
-          ensure  => $ensure_package,
-          require => Exec['chronograf_apt_get_update'],
-        }
-      }
-
-      'Debian': {
-      }
-
-      'CentOS': {
-      }
-
-      default: {
-        fail('Only Ubuntu, Debian and CentOS are supported')
+  if $facts['os']['family'] == 'Debian' {
+    if $manage_repo {
+      apt::source { 'influxdata':
+        comment  => 'InfluxDB repository',
+        location => "https://repos.influxdata.com/${facts['os']['name'].downcase}",
+        release  => $facts[os][distro][codename],
+        repos    => 'stable',
+        key      => {
+          'id'     => '05CE15085FC09D18E99EFB22684A14CF2582E0C5',
+          'source' => 'https://repos.influxdata.com/influxdb.key',
+        },
       }
     }
-    } else {
-      exec { 'chronograf_apt_get_update':
-        command     => 'apt-get update',
-        cwd         => '/tmp',
-        path        => ['/usr/bin'],
-        require     => Apt::Source['influxdata'],
-        subscribe   => Apt::Source['influxdata'],
-        refreshonly => true,
-      }
-
-      package { $package_name:
-        ensure  => $ensure_package,
-        require => Exec['chronograf_apt_get_update'],
-      }
+    include apt
+    Class['::apt::update'] -> Package[$package_name]
+    package { $package_name:
+      ensure => $ensure_package,
+    }
   }
+
 }
