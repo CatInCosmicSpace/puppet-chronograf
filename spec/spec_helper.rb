f24870868b7fd47423a9f5abbbb113e33bec8346
Woth-Ursula <uwoth@kogito.network>
2020-07-10 14:54:17 +0200

    Update pdk-version 1.18.0
    Signed-off-by: Woth-Ursula <uwoth@kogito.network>

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -34,11 +34,11 @@ end
 
 RSpec.configure do |c|
   c.default_facts = default_facts
-  c.hiera_config = File.expand_path(File.join(__FILE__, '../fixtures/hiera.yaml'))
   c.before :each do
     # set to strictest setting for testing
     # by default Puppet runs at warning level
     Puppet.settings[:strict] = :warning
+    Puppet.settings[:strict_variables] = true
   end
   c.filter_run_excluding(bolt: true) unless ENV['GEM_BOLT']
   c.after(:suite) do
