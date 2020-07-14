ac71dce76ae87e3d66bd0bc285cc2eb0d0c42244
Woth-Ursula <uwoth@kogito.network>
2020-07-14 17:31:37 +0200

    Fixed params for defaults no undef
    Signed-off-by: Woth-Ursula <uwoth@kogito.network>

--- manifests/params.pp
+++ manifests/params.pp
@@ -38,11 +38,11 @@ class chronograf::params (
   Hash $connection_kapacitor = {},
   String $kapacitor_connection_template = 'chronograf/kapacitor_connection.erb',
 
-  Optional[String] $default_host = undef,
-  Optional[String] $default_port = undef,
-  Optional[String] $default_tls_certificate = undef,
-  Optional[String] $default_token_secret = undef,
-  Optional[String] $default_log_level = undef,
+  Optional[String] $default_host = '',
+  Optional[String] $default_port = '',
+  Optional[String] $default_tls_certificate = '',
+  Optional[String] $default_token_secret = '',
+  Optional[String] $default_log_level = '',
 ){
 
 }
